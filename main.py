b7754a6e71869af1395ecbb01640db3c488266dd
abrgzej <dantumonskiy@gmail.com>
2021-02-17 21:01:28 +0300

    Finished 3 task

--- main.py
+++ main.py
@@ -48,12 +48,54 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.lbl_image.setPixmap(self.pixmap)
 
     def keyPressEvent(self, event):
+        di = {
+            0: 0.0,
+            1: 62.5,
+            2: 62.5,
+            3: 62.5,
+            4: 8.5,
+            5: 2.5,
+            6: 1.5,
+            7: 0.5,
+            8: 0.3,
+            9: 0.1,
+            10: 0.06,
+            11: 0.02,
+            12: 0.012,
+            13: 0.004,
+            14: 0.0024,
+            15: 0.0008,
+            16: 0.00048,
+            17: 0.00016
+        }
+        self.delta_x = self.delta_y = di[self.z]
         if event.key() == Qt.Key_PageUp:
             if self.z != 17:
                 self.z += 1
         if event.key() == Qt.Key_PageDown:
             if self.z != 0:
                 self.z -= 1
+        if event.key() == Qt.Key_Up:
+            if float(self.y) + self.delta_y <= 85:
+                self.y = float(self.y)
+                self.y += self.delta_y
+                self.y = str(self.y)
+        if event.key() == Qt.Key_Down:
+            if float(self.y) - self.delta_y >= -85:
+                self.y = float(self.y)
+                self.y -= self.delta_y
+                self.y = str(self.y)
+        if event.key() == Qt.Key_Left:
+            if float(self.x) - self.delta_x >= -180:
+                self.x = float(self.x)
+                self.x -= self.delta_x
+                self.x = str(self.x)
+        if event.key() == Qt.Key_Right:
+            if float(self.x) + self.delta_x <= 180:
+                self.x = float(self.x)
+                self.x += self.delta_x
+                self.x = str(self.x)
+        print(self.z)
         self.getImage()
         self.initUI()
 
